0a7cd560092b99d4c4d8ec5dbedac79ac4e33b90
Jack Balster <107078414+jack-balster@users.noreply.github.com>
2023-11-13 12:13:21 -0800

    Update auth_forms.py

--- smileApp_jack-balster/app/Controller/auth_forms.py
+++ smileApp_jack-balster/app/Controller/auth_forms.py
@@ -1,3 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import  ValidationError, DataRequired, EqualTo, Length,Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import  ValidationError, DataRequired, EqualTo, Length, Email
+from app.Model.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(max=128)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already taken. Please choose another one.')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email address already registered. Please use a different one.')
+
+
+
